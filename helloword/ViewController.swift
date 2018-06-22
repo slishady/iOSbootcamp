045d80002d7683cd3df7483b960aa760bc8777c3
xiaobowen <931759898@qq.com>
2018-06-22 16:26:34 +0800

    Initial Commit

---
+++ helloword/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloword
+//
+//  Created by 肖博文 on 2018/6/22.
+//  Copyright © 2018年 117010300. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ hellowordTests/hellowordTests.swift
@@ -0,0 +1,36 @@
+//
+//  hellowordTests.swift
+//  hellowordTests
+//
+//  Created by 肖博文 on 2018/6/22.
+//  Copyright © 2018年 117010300. All rights reserved.
+//
+
+import XCTest
+@testable import helloword
+
+class hellowordTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
